7f80b5f8609e22fe0ed8770d9fb6148159de6b70
Mohammad Mohsen Ghannam <mohammad.mohsen@hotmail.com>
2017-10-14 20:12:54 +0200

    sheet3

---
+++ helpers.py
@@ -0,0 +1,58 @@
+from numpy import mean, array
+from numpy.linalg import norm
+
+
+def euclidean_distance(a, b):
+    return norm(a - b)
+
+
+def manhattan_distance(a, b):
+    return sum(abs(xn - yn) for xn, yn in zip(a, b))
+
+
+def k_means(data, k, error, distance_func=euclidean_distance, max_iter=100):
+    import random
+    t = 0
+    assignment = {}
+    for _ in range(k):
+        assignment[tuple(random.choice(data))] = []
+
+    while True:
+        t += 1
+        # cluster assignment step
+        for sample in data:
+            closest_mean = min(assignment.keys(), key=lambda x: distance_func(array(x), sample))
+            assignment[closest_mean].append(sample)
+        # centroid update step
+        for last_mean in [key for key in assignment.keys()]:
+            new_mean = mean(assignment[last_mean], axis=0)
+            assignment[tuple(new_mean)] = assignment[last_mean]
+            del assignment[last_mean]
+
+        if sum_square_error(assignment) < error or t > max_iter:
+            break
+    return assignment
+
+
+def sum_square_error(assignment):
+    error = 0
+    for mean in assignment.keys():
+        for element in assignment[mean]:
+            error += norm(element - mean) ** 2
+    # print(error)
+    return error
+
+
+def scatter_plot_assignment(assignment):
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    axis = Axes3D(fig)
+    colors = ['red', 'blue', 'green']
+    color_index = 0
+    for centroid in assignment.keys():
+        axis.scatter(*assignment[centroid], color=colors[color_index])
+        color_index += 1
+
+    plt.show()
---
+++ sheet3.py
@@ -0,0 +1,33 @@
+# 2-a,b
+
+from numpy import array
+from helpers import k_means, scatter_plot_assignment
+
+data = array([[0.5, 4.5, 2.5],
+              [2.2, 1.5, 0.1],
+              [3.9, 3.5, 1.1],
+              [2.1, 1.9, 4.9],
+              [0.5, 3.2, 1.2],
+              [0.8, 4.3, 2.6],
+              [2.7, 1.1, 3.1],
+              [2.5, 3.5, 2.8],
+              [2.8, 3.9, 1.5],
+              [0.1, 4.1, 2.9]])
+
+assignment = k_means(data, 3, 27)
+# scatter_plot_assignment(assignment)
+
+
+# 2-c
+from helpers import manhattan_distance
+
+assignment_manhattan = k_means(data, 3, 27, manhattan_distance)
+# scatter_plot_assignment(assignment_manhattan)
+
+# 2-d
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+normalized_data = scaler.fit_transform(data)
+
+assignment_normalized = k_means(normalized_data, 3, 1)
+# scatter_plot_assignment(assignment_normalized)
